9eb31659f627f617d22a6b05a6c8b850cc8ee8cc
Roopa <Roopa@ROOPA>
2022-05-30 23:20:54 -0700

    withoutparameter

---
+++ src/test/java/com/jenkins/WithoutParameter/PractiseWithoutParameter.java
@@ -0,0 +1,14 @@
+package com.jenkins.WithoutParameter;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PractiseWithoutParameter
+{
+	 @Test
+	 public void teststep1Test()
+	 {
+		 Reporter.log("test case1",true);
+	 }
+	
+}
